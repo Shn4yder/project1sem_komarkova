7a373b903ac3cee1060b1273fafa45ad34d1ad78
Shn4yder <64658109+Shn4yder@users.noreply.github.com>
2021-11-21 11:17:01 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -3,7 +3,7 @@
 """
 
 
-def generator(a, b, c, d):
+def generator(a, b, c, d):                                 # реализация функции-генератора 
     number = str(a) + str(b) + str(c) + str(d)
     if (a == b) or (a == c) or (a == d ) or (b == c) or (b == d) or (c == d):
         ravenstvo = 'В Вашем числе есть одинаковые цифры'
@@ -19,7 +19,8 @@ num3 = int(input('Введите третью цифру  '))
 num4 = int(input('Введите четвертую цифру  '))
 
 
-generator(num1, num2, num3, num4)
+generator(num1, num2, num3, num4)                         # использование функции от введенных данных
+
 
 
 
