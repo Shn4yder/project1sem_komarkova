a570649cb38aa103525ea7410e701600d19912ca
Shn4yder <64658109+Shn4yder@users.noreply.github.com>
2021-11-22 08:58:14 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -15,9 +15,9 @@ while type(n) != int:           # обработка исключений для
 
 if n > 0:
     summa = 0
-    while n > 0:
+    while n > 0:                 # подсчет суммы 1 + 1/2 + 1/2 + ... + 1/N
         summa = summa + 1/n
         n = n - 1
     print('Сумма равна:', summa)
 else:
-    print('Число N не должно равняться нулю')
+    print('Число N не должно быть меньше 0')
