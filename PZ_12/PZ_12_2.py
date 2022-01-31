8fd2e436b9611a9a5bdc45390223f467a07df9fc
Semyon <litvinatya7@mail.ru>
2022-01-31 17:58:11 +0300

    ПЗ 12

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,52 +1,47 @@
+"""
+Дан целочисленный список размера 10. Вывести вначале все содержащиеся в данном списке четные числа в порядке возрастания
+их индексов, а затем — все нечетные числа в порядке убывания их индексов.
+"""
 from tkinter import *
 
 root = Tk()
-root.title('Генератор чисел')
+root.title('Четные/ нечетные элементы списка')
 root.geometry('600x200+400+200')
 
-def generator(event):
-    a = num_1.get()
-    b = num_2.get()
-    c = num_3.get()
-    d = num_4.get()
+def obrabotchik(event):
+    list = [int(i) for i in l.get().split(', ')]
+    print(list)
 
-    number = a + b + c + d
-    if (a == b) or (a == c) or (a == d) or (b == c) or (b == d) or (c == d):
-        ravenstvo = 'есть одинаковые цифры'
-    else:
-        ravenstvo = 'нет одинаковых цифр'
+    list_chet = []
+    list_nechet = []
 
-    sravnenie['text'] = "В Вашем числе", ravenstvo
-    num['text'] = "Ваше число", number
+    for element in list:  # перебор элементов. Сбор четных
+        if element % 2 == 0:
+            list_chet.append(element)
 
+    for element in list[::-1]:  # перебор элементов. Сбор нечетных с конца
+        if element % 2 == 1:
+            list_nechet.append(element)
 
+    chet['text'] = f"Четные элемениы Вашего списка: {list_chet}"
+    nechet['text'] = f"Нечетные элементы Вашего списка с конца: {list_nechet}"
 
-Label(text="Первое число").grid(row=0, column=0)
-num_1 = Entry()
-num_1.grid(row=0, column=1)
-Label(text="Второе число").grid(row=1, column=0)
-num_2 = Entry()
-num_2.grid(row=1, column=1)
-Label(text="Третье число").grid(row=2, column=0)
-num_3 = Entry()
-num_3.grid(row=2, column=1)
-Label(text="Четвертое число").grid(row=3, column=0)
-num_4 = Entry()
-num_4.grid(row=3, column=1)
+Label(text="Введите список").grid(row=0, column=0)
+l = Entry()
+l.grid(row=0, column=1)
+
 
 button1 = Button(text="Сгенерировать!")
-button1.grid(row=4, column=1)
+button1.grid(row=1, column=1)
 
-button1.bind('<Button-1>',generator)
+button1.bind('<Button-1>', obrabotchik)
 
+chet = Label()
+chet.grid(row=2, column=1)
 
-sravnenie = Label()
-sravnenie.grid(row=5, column=1)
-
-num = Label()
-num.grid(row=6, column=1)
-
-root.mainloop()
-
+nechet = Label()
+nechet.grid(row=3, column=1)
 
 root.mainloop()
+
+
