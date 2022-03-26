6f813d29bfdbb5c4f947cbb13d271c696f6abcb1
Shn4yder <litvinastya7@mail.ru>
2022-03-26 17:31:00 +0300

    пз 15

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -9,11 +9,14 @@ n = int(input("Введите число столбцов матрицы "))
 
 matrix = [[randint(0, 10) for j in range(n)] for i in range(m)]
 list = [randint(-10, 0) for x in range(m)]
-print(matrix)
-print(list)
+print('Список для обновления матрицы', list)
+print('Исходная матрица')
+for i in matrix:
+    print(*i)
+
 for i in range(m):
-    print('Элемент матрицы', matrix[i][1])
-    print(list[i])
     matrix[i][1] = list[i]
 
-print(matrix)
+print('Обновленная матрица')
+for i in matrix:
+    print(*i)
--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -6,16 +6,18 @@ n = int(input("Введите число строк матрицы "))
 m = int(input("Введите число столбцов матрицы "))
 
 matrix = [[randint(-10, 10) for j in range(n)] for i in range(m)]
-print(matrix)
-h = []
+print('Исходная матрица')
+for i in matrix:
+    print(*i)
 
+three = []
 for i in matrix:
     for element in i:
         if element > 0 and element % 3 == 0:
-            h.append(element)
+            three.append(element)
 
-if len(h) == 0:
+if len(three) == 0:
     print('Элементов кратных трем не встретилось')
 else:
-    print('Среднее арифметическое положительных элементов:', sum(h) / len(h))
+    print('Среднее арифметическое положительных элементов:', sum(three) / len(three))
 
